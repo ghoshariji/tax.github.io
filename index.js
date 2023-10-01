edff590e5cc04d3df721c81dcbea8d8b236e6076
ghoshariji <144580430+ghoshariji@users.noreply.github.com>
2023-10-01 18:25:35 +0530

    well done

---
+++ index.js
@@ -0,0 +1,34 @@
+let tcars = 0;
+let tcash = 0;
+let count=0;
+
+
+
+function calculateTax()
+{
+    document.getElementById('result1').textContent=tcars;
+    document.getElementById('result2').textContent=tcash;
+
+}
+
+function calculateTax1()
+{
+alert("paying");
+count++;
+document.getElementById('count').textContent=count;
+++tcars;
+tcash+=100;
+}
+
+function calculateTax2()
+{
+    alert("Non paying");
+    document.getElementById('count1').textContent=count;
+    ++tcars;
+}
+
+
+
+ document.getElementById('ab').addEventListener('click',calculateTax1);
+ document.getElementById('abcd').addEventListener('click',calculateTax2);
+document.getElementById('display').addEventListener('click',calculateTax);
